4f61438eefea16a36acd022ff0bb7a432ef14502
bartolomeoadrian <bartolomeoadrian@hotmail.com>
2024-03-29 23:21:26 -0300

    Avances

---
+++ python/simple.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+import chromadb
+from llama_index.core import  SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.core import Settings
+from llama_index.llms.ollama import Ollama
+
+# log
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# llm
+llm = Ollama(model="llama2", request_timeout=60.0)
+response  = llm.complete("Cual es la ley 23349")
+print(response)
