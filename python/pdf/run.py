2f022ecc72fac55424a76818b6b65a681536a4d9
bartolomeoadrian <bartolomeoadrian@hotmail.com>
2024-04-11 22:27:06 -0300

    Avance PDF

---
+++ python/pdf/run.py
@@ -0,0 +1,42 @@
+import logging
+import sys
+import chromadb
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.core import Settings
+from llama_index.llms.ollama import Ollama
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
+
+# log
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# data
+documents = SimpleDirectoryReader("data/pdf").load_data()
+
+# vector store
+vector_store = ElasticsearchStore(
+    index_name="pdf", es_url="http://localhost:9200"
+)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+
+# embedding
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+index = VectorStoreIndex.from_documents(
+    documents, storage_context=storage_context, show_progress=True
+)
+
+# query
+query_engine = index.as_query_engine(
+    streaming=True, llm=Ollama(model="mistral", request_timeout=120.0)
+)
+
+while True:
+    query = input("Query: ")
+    response_stream = query_engine.query(query + "\nRespond in Spanish\n")
+    response_stream.print_response_stream()
+    print("------")
--- python/txt/run.py
+++ python/txt/run.py
@@ -7,23 +7,21 @@ from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
 from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
 
 # log
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-# llm
-Settings.llm = Ollama(model="llama2", request_timeout=60.0)
-
 # data
 documents = SimpleDirectoryReader("data/txt").load_data()
 
 # vector store
-db = chromadb.PersistentClient(path="./chroma")
-chroma_collection = db.get_or_create_collection("ollama")
-
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+vector_store = ElasticsearchStore(
+    index_name="txt", es_url="http://localhost:9200"
+)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
 
 # embedding
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
@@ -34,7 +32,7 @@ index = VectorStoreIndex.from_documents(
 
 # query
 query_engine = index.as_query_engine(
-    streaming=True, llm=Ollama(model="llama2", request_timeout=120.0)
+    streaming=True, llm=Ollama(model="mistral", request_timeout=120.0)
 )
-response_stream = query_engine.query("De que trata la noticia?")
+response_stream = query_engine.query("De que trata la noticia?\nRespond in Spanish\n")
 response_stream.print_response_stream()
