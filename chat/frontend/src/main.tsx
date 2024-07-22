079bbcb3e3ec75d61e8516f48b36fc4c9cefaeaf
bartolomeoadrian <bartolomeoadrian@hotmail.com>
2024-07-22 17:48:07 -0300

    Mejoras en el compose

--- chat/frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import "react-chat-elements/dist/main.css"
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<App />,
-)
---
+++ docker/iada/src/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Chat from './components/Chat'
+
+function App() {
+	return (
+		<div style={{
+			width: "100dvw",
+			height: "100dvh",
+			padding: "20px",
+		}}>
+			<Chat />
+		</div>
+	)
+}
+
+export default App
