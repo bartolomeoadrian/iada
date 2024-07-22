264c349cfd1a71e6eb49900823e26b67d333e09b
bartolomeoadrian <bartolomeoadrian@hotmail.com>
2024-07-22 19:32:29 -0300

    Mejoras frontend

--- docker/iada/src/frontend/src/main.tsx
+++ docker/iada/src/frontend/src/main.tsx
@@ -1,9 +1,31 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import "react-chat-elements/dist/main.css"
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Proyects from './pages/proyects/Proyects'
+import Navigator from './pages/navigator/Navigator'
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <></>,
+	},
+	{
+		path: "/proyects",
+		element: <Proyects />,
+	},
+	{
+		path: "/navigator",
+		element: <Navigator />,
+	},
+], {
+	basename: "/chat",
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<App />,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
