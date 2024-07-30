7eaefcca216e69561a83df4cfb1e8343ebfca37e
bartolomeoadrian <bartolomeoadrian@hotmail.com>
2024-07-30 00:17:58 -0300

    Imagen de Docker

--- docker/iada/src/frontend/vite.config.ts
+++ docker/iada/src/frontend/vite.config.ts
@@ -4,22 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	base: "./",
 	plugins: [react()],
-	preview: {
-		port: 3000,
-		strictPort: true,
-	},
 	server: {
-		port: 3000,
-		strictPort: true,
-		host: true,
-		origin: "http://0.0.0.0:3000",
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				secure: false,
-				ws: true,
-			}
-		}
+		watch: {
+			usePolling: true,
+		},
 	},
 });
